5bee22c8c7080b7251647873f69ca56ec6eb92d6
MpumiSuccess <nompumelelosuccess9@gmail.com>
2023-06-18 22:54:14 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -4,25 +4,23 @@
 
 /**
  * main - Entry point
- * Remain: Always 0
+ * Return: Always 0
  */
 
-int main(void) 
-
+int main(void)
 
 {
 
-
 	int p, q;
 
-	for (p = 0; p <= 98; p++)
+	for (p = 0; <= 98; p++)
 	{
 
 	for (q = p + 1; q <= 99; q++)
 	{
 	putchar((p / 10) + '0');
 	putchar((p % 10) + '0');
-	putchar (' ');
+	putchar(' ');
 	putchar((q / 10) + '0');
 	putchar((q % 10) + '0');
 	if (p == 98 && q == 99)
