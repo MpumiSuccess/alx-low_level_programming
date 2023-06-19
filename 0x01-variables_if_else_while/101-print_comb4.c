58ce4a8858b7126998778821efde01502ccd5a19
MpumiSuccess <nompumelelosuccess9@gmail.com>
2023-06-18 23:10:20 -0700

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,12 +10,13 @@
 int main(void)
 
 {
+
 	int d, p, q;
 
 	for (d = '0'; d < 'g'; d++)
 	{
 	
-	for (p = d = 1; p <= '9'; p++)
+	for (p = d + 1; p <= '9'; p++)
 	{
 
 	for (q = p + 1; q <= '9'; q++)
