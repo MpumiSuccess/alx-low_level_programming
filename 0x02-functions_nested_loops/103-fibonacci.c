9cb5abb90a5d59ee7e99e45b04bd1248cd534c81
MpumiSuccess <nompumelelosuccess9@gmail.com>
2023-06-21 02:43:52 -0700

    task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -12,7 +12,7 @@ int main(void)
 
 	while (i < 50)
 	{
-		if (i ==0)
+		if (i == 0)
 			printf("%ld", j);
 		else if (i == 1)
 			printf(", %ld", k);
@@ -22,7 +22,6 @@ int main(void)
 			j = k - j;
 			printf(", %ld", k);
 		}
-		
 		++i;
 	}
 	printf("\n");
