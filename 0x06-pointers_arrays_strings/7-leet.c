b2704e351eef753eedbe8c35d9ed6f1e6de33fee
MpumiSuccess <nompumelelosuccess9@gmail.com>
2023-06-29 03:26:44 -0700

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,21 +6,22 @@
  */
 char *leet(char *n)
 {
-	int i, j;
-	char s1[] = "aAeEoOtTlL";
-	char s2[] = "4433007711";
+int i, j;
+char s1[] = "aAeEoOtTlL";
+char s2[] = "4433007711";
 
-	for (i = 0; n[i] != '\0'; j++)
-	{
-		for (j = 0; j < 10; j++)
+for (i = 0; n[i] != '\0'; j++)
 
-		{
+{
 
-		if (n[i] == s1[j])
-			{
-				n[i] = s2[j];
-			}
-		}
-	}
-	return (n);
+for (j = 0; j < 10; j++)
+{
+
+if (n[i] == s1[j])
+{
+n[i] = s2[j];
+}
+}
+}
+return (n);
 }
