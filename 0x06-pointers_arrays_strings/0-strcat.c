1a4d7a6f401c55a1777da0a62c40db196c3add2c
MpumiSuccess <nompumelelosuccess9@gmail.com>
2023-06-30 04:26:12 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,13 +1,12 @@
 #include "main.h"
-
 /**
  * _strcat - concatenates two strings
  * @dest: input value
- * @src: inpuyt value
+ * @src: input value
+ *
  * Return: void
  */
 char *_strcat(char *dest, char *src)
-
 {
 	int i;
 	int j;
