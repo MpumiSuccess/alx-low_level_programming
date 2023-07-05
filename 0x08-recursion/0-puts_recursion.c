1a26d4840770d67fe9e432e9ca308eecd0f2536f
MpumiSuccess <nompumelelosuccess9@gmail.com>
2023-07-05 02:43:54 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _puts_recursion - Print a string followed by a new line 
+ * _puts_recursion - Print a string followed by a new line
+ *
  * @s: the string to print
  *
  * Return: Nothing.
