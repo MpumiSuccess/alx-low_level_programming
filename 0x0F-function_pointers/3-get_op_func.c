356743ee6388d4ae60692e361a0f100ee0e6e71b
MpumiSuccess <nompumelelosuccess9@gmail.com>
2023-07-19 03:46:25 -0700

    task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -17,11 +17,11 @@ int (*get_op_func(char *s))(int, int)
 	{ "/", op_div },
 	{ "%", op_mod },
 	{ NULL, NULL }
-        };
-        int i = 0;
+	};
+	int i = 0;
 
 	while (i < 5)
-        {
+	{
 		if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);
 
